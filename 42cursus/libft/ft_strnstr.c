31d305c537e31d86ac3b568369ab2cb0c9ce064c
Ali Baris Okur <aokur@k1m34s04.42istanbul.com.tr>
2025-05-28 19:41:06 +0300

    libftv2

--- 42cursus/libft/ft_strnstr.c
+++ 42cursus/libft/ft_strnstr.c
@@ -6,7 +6,38 @@
 /*   By: aokur <aokur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 17:25:27 by aokur             #+#    #+#             */
-/*   Updated: 2025/05/26 17:56:36 by aokur            ###   ########.fr       */
+/*   Updated: 2025/05/28 19:40:41 by aokur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
+#include <stdio.h>
+
+char	*ft_strnstr(const char *a, const char *b, size_t n)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (b[i] == '\0')
+		return ((char *)a);
+	while (a[i] && i < n)
+	{
+		j = 0;
+		while (a[i+j] && b[j] && a[i + j] == b[j] && (i + j) < n)
+		{
+			if (b[j + 1] == '\0')
+				return ((char *)&a[i]);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
+int main()
+{
+	char *a = "alibarisokur";
+	char *b = "alib";
+	printf("%s", ft_strnstr(a,b,4));
+	
+}
