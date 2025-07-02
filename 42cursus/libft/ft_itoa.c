bae96f730c36fb73a624b0e1546c13b25d41cf2e
Ali Baris Okur <aokur@k1m01s06.42istanbul.com.tr>
2025-07-02 18:15:11 +0300

    libftv10

--- 42cursus/libft/ft_itoa.c
+++ 42cursus/libft/ft_itoa.c
@@ -6,22 +6,12 @@
 /*   By: aokur <aokur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 17:59:52 by aokur             #+#    #+#             */
-/*   Updated: 2025/06/28 18:52:32 by aokur            ###   ########.fr       */
+/*   Updated: 2025/07/02 18:13:24 by aokur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*itoa_str(int n)
-{
-	char	*a;
-	size_t	i;
-
-	while(n)
-	{
-		a[i] = 
-	}
-}
 
 int	itoa_tmp(int n)
 {
@@ -41,19 +31,42 @@ int	itoa_tmp(int n)
 	return (i);
 }
 
+char	*itoa_str(int n)
+{
+	char	*a;
+	int	i;
+	int	b;
+
+	b = itoa_tmp(n);	
+	a = malloc(b * sizeof(char));
+	i = 0;
+	if (n < 0)
+	{
+		n = n * -1;
+		a[i] = '-';
+		i++;
+	}
+	while (i < b - 1)
+		i++;
+	while(i > 0)
+	{
+		a[i] = (n % 10) + 48;
+		n  = n / 10;
+		i--;
+		
+	}
+	return (a);
+}
+
 char	*ft_itoa(int n)
 {
 	char	*tmp;
-	size_t	i;
+	int	i;
 
 	tmp = malloc(itoa_tmp(n) * (sizeof(char)));
 	i = 0;
 	if (!tmp)
 		return (NULL);
-	while (i < n)
-	{
-		tmp[i] = (char);
-		i++;
-	}
+	tmp = itoa_str(n);
 	return (tmp);
 }
